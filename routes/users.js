f29079c3cd085666a74944f5587148ac451b32cf
Kevin C McKenna <kevmckenna@gmail.com>
2017-10-05 17:27:02 -0700

    initial commit / routes made

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,15 @@
-//routes here
+const 
+    express = require('express')
+    videosRouter = new express.Router()
+    User = require('../models/User.js')
+    usersCtrl = require('../controllers/users.js')
+
+usersRouter.route('/')
+    .get(usersCtrl.index)
+    .post(usersCtrl.create)
+usersRouter.route('/:id')
+    .get(usersCtrl.show)
+    .patch(usersCtrl.update)
+    .delete(usersCtrl.destroy)
+
+module.exports = usersRouter
